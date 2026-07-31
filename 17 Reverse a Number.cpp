d8b08c850aa701f8966fbe7eab1d9bedb5d0936e
vardhanmajalekar21-gif <vardhanmajalekar21@gmail.com>
2026-07-31 12:15:57 +0530

    17 Reverse a Number.cpp

---
+++ 17 Reverse a Number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, rev = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+    while (n != 0) 
+    {
+        rev = rev * 10 + n % 10;
+        n = n / 10;
+    }
+    cout << "Reverse = " << rev;
+    return 0;
+}

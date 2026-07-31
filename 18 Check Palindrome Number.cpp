ce05859461e462628cb03ca38828ffbd12cd18d9
vardhanmajalekar21-gif <vardhanmajalekar21@gmail.com>
2026-07-31 12:26:17 +0530

    Add files via upload

---
+++ 18 Check Palindrome Number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, temp, rev = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+    temp = n;
+    while (n != 0) 
+    {
+        rev = rev * 10 + n % 10;
+        n = n / 10;
+    }
+    if (temp == rev)
+        cout << "Palindrome Number";
+    else
+        cout << "Not a Palindrome Number";
+    return 0;
+}

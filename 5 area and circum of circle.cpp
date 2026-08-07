cce0ea5389a86cc3dbf9357caaa9b66e914e8325
vardhanmajalekar21-gif <vardhanmajalekar21@gmail.com>
2026-08-07 11:52:25 +0530

    Update 5 area and circum of circle.cpp

--- 5 area and circum of circle.cpp
+++ 5 area and circum of circle.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main()
+{
     float radius;
-
     cout << "Enter radius: ";
     cin >> radius;
-
     cout << "Area = " << 3.14 * radius * radius << endl;
     cout << "Circumference = " << 2 * 3.14 * radius;
-
     return 0;
 }

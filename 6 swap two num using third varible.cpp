01aec1beb1bcbf9465a512c070ab22c3491f85b2
vardhanmajalekar21-gif <vardhanmajalekar21@gmail.com>
2026-08-07 11:53:19 +0530

    Update 6 swap two num using third varible.cpp

--- 6 swap two num using third varible.cpp
+++ 6 swap two num using third varible.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main() 
+{
     int a, b, temp;
-
     cout << "Enter two numbers: ";
     cin >> a >> b;
-
     temp = a;
     a = b;
     b = temp;
-
     cout << "After swapping:\n";
     cout << "a = " << a << endl;
     cout << "b = " << b;
-
     return 0;
 }
